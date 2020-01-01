be7def3ed8791a411d526b4301393b851dd045e0
yguozhen <47097105+yguozhen@users.noreply.github.com>
2019-12-31 23:19:17 -0600

    Create exercise1a.java

---
+++ HW0/exercise1a.java
@@ -0,0 +1,11 @@
+public class Test {
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
